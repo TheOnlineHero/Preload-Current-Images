f8b07fccf8250e7fe56a555724c61d48123cecd1
Tom Skroza <tom.skroza@marketingmix.com.au>
2013-03-07 13:32:02 +0800

    Also included a progress bar that you can use to tell user all images are fully loaded.

--- js/application.js
+++ js/application.js
@@ -1,3 +1,10 @@
+jQuery(window).load(function() {
+  jQuery("body").attr("loaded_image_count_on_page", jQuery("body").attr("image_count_on_page"));
+});
+
+var image_count_on_page = 0;
+var loaded_image_count_on_page = 0;
+
 (function($) {
   var cache = [];
   // Arguments are image paths relative to the current page.
@@ -6,12 +13,19 @@
     for (var i = args_len; i--;) {
       var cacheImage = document.createElement('img');
       cacheImage.src = arguments[i];
+      jQuery(cacheImage).load(function() {
+        loaded_image_count_on_page++;
+        jQuery("body").attr("loaded_image_count_on_page", loaded_image_count_on_page);
+      });
       cache.push(cacheImage);
+      image_count_on_page++;
+      jQuery("body").attr("image_count_on_page", image_count_on_page);
     }
   }
 })(jQuery);
 
 jQuery(function() {
+
   jQuery("*").each(function() {
     if (jQuery(this).css("background-image") != "" && jQuery(this).css("background-image") != "none") {
 
@@ -19,9 +33,8 @@ jQuery(function() {
       // ^ Either "none" or url("...urlhere..")
       bg_url = bg_url.match(/^url\(['"](.+)["']\)$/);
       bg_url = bg_url ? bg_url[1] : ""; // If matched, retrieve url, otherwise ""
-
       jQuery.preLoadImages(bg_url);
-      console.log(bg_url);
+
     }
   });
   jQuery("img").each(function() {
--- preload-current-images.php
+++ preload-current-images.php
@@ -21,11 +21,51 @@ Author: TheOnlineHero - Tom Skroza
 License: GPL2
 */
 
+add_shortcode( 'preload_progress_bar', 'preload_progress_bar_shortcode' );
+
+function preload_progress_bar_shortcode($atts) {
+	return "<script language='javascript'>
+	jQuery(function() {
+	  var progressbar = jQuery( '#progressbar' ),
+	    progressLabel = jQuery( '.progress-label' );
+
+	  progressbar.progressbar({
+	    value: false,
+	    change: function() {
+	      progressLabel.text( progressbar.progressbar( 'value' ) + '%' );
+	    },
+	    complete: function() {
+	      ".$atts["complete"]."
+	    }
+	  });
+
+	  function progress() {
+	    var val = progressbar.progressbar( 'value' ) || 0;
+
+	    //progressbar.progressbar( 'value', val + 1 );
+	    if (jQuery('body').attr('loaded_image_count_on_page')) {
+	      progressbar.progressbar( 'value', parseInt((jQuery('body').attr('loaded_image_count_on_page') / jQuery('body').attr('image_count_on_page')) * 100) );  
+	    }
+	    
+	    if ( val < 99 ) {
+	      setTimeout( progress, 250 );
+	    }
+	  }
+
+	  setTimeout( progress, 3000 );
+	});
+  </script><div id='progressbar'><div class='progress-label'>Loading...</div></div>";
+}
+
 add_action('wp_head', 'add_preload_current_images_js');
 function add_preload_current_images_js() { 
   wp_enqueue_script('jquery');
   wp_register_script( 'my-jquery-preload-images', plugins_url("/js/application.js", __FILE__) );
   wp_enqueue_script('my-jquery-preload-images');
+  wp_register_style("jquery-ui", "http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css");
+  wp_enqueue_style("jquery-ui");
+  wp_register_style("my-preload-progress-bar", plugins_url("/css/style.css", __FILE__));
+  wp_enqueue_style("my-preload-progress-bar");
 }
 
 ?>
